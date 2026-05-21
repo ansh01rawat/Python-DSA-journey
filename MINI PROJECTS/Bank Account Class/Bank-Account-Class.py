6ca09578d430e4ee9c1c02f65d57b9ce50656419
Ansh Rawat <anshrawat200708@gmail.com>
2026-05-21 13:51:20 +0530

    Update Bank-Account-Class.py

--- MINI PROJECTS/Bank Account Class/Bank-Account-Class.py
+++ MINI PROJECTS/Bank Account Class/Bank-Account-Class.py
@@ -1,19 +1,18 @@
-def display_balance():
-    print("")
-
-
 class BankAccount:
     def __init__(self,name,balance):
         self.name = name
         self.balance = balance
 
     def deposit(self,amount):
+        if amount > 0
         self.balance += amount
 
     def withdraw(self,amount):
 
         if self.balance > amount:
             self.balance -= amount
+        elif amount <= 0:
+            print("invalid amount")
         else:
             print("insufficient balance")
 
@@ -22,8 +21,14 @@ class BankAccount:
         print("balance: ",self.balance)
 
 account1 = BankAccount("Mukesh",100000000000)
+account2 = BankAccount("Tata",1000000000)
 
 account1.display_balance()
 account1.deposit(300000000000)
 account1.withdraw(21000000)
 account1.display_balance()
+
+account2.display_balance()
+account2.deposit(300000000000)
+account2.withdraw(21000000)
+account2.display_balance()
