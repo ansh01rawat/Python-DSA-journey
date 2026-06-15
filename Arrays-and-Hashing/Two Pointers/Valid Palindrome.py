c25d079ca0eff28d5b79dc2faa27659420f011b6
Ansh Rawat <anshrawat200708@gmail.com>
2026-06-15 19:57:33 +0530

    Add files via upload

---
+++ Arrays-and-Hashing/Two Pointers/Valid Palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        result = ""
+
+        for char in s:
+            if char.isalnum():
+                result += char.lower()
+        if s == " ":
+            return True
+        left = 0
+        right = len(result) - 1
+        while left < right:
+            if result[left] != result[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
