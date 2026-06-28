c6142770608f2dd2d248aa845ddf283d1ea2c937
Ansh Rawat <anshrawat200708@gmail.com>
2026-06-28 21:54:21 +0530

    Add files via upload

---
+++ linkedList/intersectionOfTwoLinkedList.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        ptrA = headA
+        ptrB = headB
+        while ptrA != ptrB:
+            if ptrA is not None:
+                ptrA = ptrA.next
+            else:
+                ptrA = headB
+            if ptrB is not None:
+                ptrB = ptrB.next
+            else:
+                ptrB = headA
+        return ptrA
