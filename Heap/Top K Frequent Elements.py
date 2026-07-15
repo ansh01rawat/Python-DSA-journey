8afd6bb397a9773761fa51581e87314f2287c41a
Ansh Rawat <anshrawat200708@gmail.com>
2026-07-15 11:20:42 +0530

    Add files via upload

---
+++ Heap/Top K Frequent Elements.py
@@ -0,0 +1,12 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = {}
+        heap = []
+        heapify(heap)
+        for num in nums:
+            freq[num] = freq.get(num,0) + 1
+        for num,count in freq.items():
+            heappush(heap,(count,num))
+            if len(heap)>k:
+                heappop(heap)
+        return [num for count,num in heap]
